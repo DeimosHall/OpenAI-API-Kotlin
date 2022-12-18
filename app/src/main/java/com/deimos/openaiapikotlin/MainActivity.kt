d37dcf6bcf63129b9e4b269b29c5eb494171ae54
Deimos <deimoshall@gmail.com>
2022-12-18 14:20:49 -0600

    Test create a library

--- app/src/main/java/com/deimos/openaiapikotlin/MainActivity.kt
+++ app/src/main/java/com/deimos/openaiapikotlin/MainActivity.kt
@@ -2,10 +2,17 @@ package com.deimos.openaiapikotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.deimos.openaiapi.Test
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMessage: Button = findViewById(R.id.btnMessage)
+        val test = Test(applicationContext)
+
+        btnMessage.setOnClickListener { test.showMessage() }
     }
 }
